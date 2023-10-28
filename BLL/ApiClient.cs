712cc27f4806076174da055e809c212af46eda10
Diego Andrés Castro <diegocastroac123@gmail.com>
2023-10-27 22:20:24 -0500

    Se inicia la capa de datos

---
+++ BLL/ApiClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace BLL
+{
+    public class ApiClient
+    {
+        private readonly RestClient client;
+        private readonly string URIbase;
+
+        public ApiClient(string URIbase)
+        {
+            client = new RestClient(URIbase);
+            this.URIbase = URIbase;
+        }
+
+        public async Task<string> GetAsync(string endpoint)
+        {
+            var request = new RestRequest(endpoint, Method.Get);
+            RestResponse response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Content;
+            }
+            else
+            {
+                throw new Exception($"Error en la solicitud: {response.ErrorMessage}");
+            }
+        }
+
+        public async Task<string> PostAsync(string endpoint, object data)
+        {
+            var request = new RestRequest(endpoint, Method.Post);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(data);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Content;
+            }
+            else
+            {
+                throw new Exception($"Error en la solicitud: {response.ErrorMessage}");
+            }
+        }
+
+        public async Task<string> PutAsync(string endpoint, object data) 
+        {
+            var request = new RestRequest(endpoint, Method.Put); // Puede ser Patch
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(data);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Content;
+            }
+            else
+            {
+                throw new Exception($"Error en la solicitud: {response.ErrorMessage}");
+            }
+        }
+
+        public async Task<string> DeleteAsync(string endpoint)
+        {
+            var request = new RestRequest(endpoint);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            if(response.IsSuccessful)
+            {
+                return response.Content;
+            }
+            else
+            {
+                throw new Exception($"Error en la solicitud: {response.ErrorMessage}");
+            }
+        }
+
+    }
+}
---
+++ BLL/Clases Abstractas/AbstractLectura.cs
@@ -0,0 +1,32 @@
+﻿using BLL.Interfaces;
+using Entity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Clases_Abstractas
+{
+    public abstract class AbstractLectura<T> : ILectura<T> where T : class
+    {
+        private readonly ApiClient apiClient;
+        private readonly string URIbase;
+
+        public AbstractLectura(ApiClient apiClient, string URIbase)
+        {
+            
+            this.apiClient = new ApiClient(URIbase);
+            this.URIbase = URIbase; 
+        }
+
+        public virtual async Task<List<T>> ObtenerTodos()
+        {
+            string jsonResponse = await apiClient.GetAsync(URIbase);
+            List<T> items = JsonConvert.DeserializeObject<List<T>>(jsonResponse);
+            return items;
+        }
+    }
+}
